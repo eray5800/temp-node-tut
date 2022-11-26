d38f65cdd918e136e80a85fa87964114a8a809de
Eray Yenigün <erayfb7@hotmail.com>
2022-11-26 22:55:54 +0300

    aa

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const http = require("http");
+const {readFile,writeFile} = require('fs').promises
+
+
+con
+const start = async() =>{
+    try {
+       const firstFile = await readFile("./content/first.txt","utf-8");
+       const secondFile = await readFile("./content/second.txt","utf-8");
+       await writeFile("./content/zimbo.txt",`bu iki mesaj : ${firstFile},${secondFile}`)
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+start();
